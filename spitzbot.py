aaae5f7f96b92210cb07c6d4ae24f62a2d96a061
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-04-02 20:31:03 +0900

    Add files via upload

---
+++ spitzbot.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+import os
+
+token = os.environ.get('DISCORD_BOT_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('起動しました')
+
+@client.event
+async def on_message(message):
+if message.content.startswith('#spitz:'):
+    kasi = message.content.replace('#spitz:', '')
+    with open(kasi) as kasii:
+        kasiyaru = kasii.read()
+    await message.channel.send(kasiyaru)
+
+
+client.run(token)
